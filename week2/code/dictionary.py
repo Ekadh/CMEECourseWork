c37dbdb9ff0495dd24f138b9a240fc08c636f86b
er925@ic.ac.uk <brinda@ekadhs-MacBook-Air.local>
2025-10-13 14:32:13 +0100

    Finished Q1 of dictionary.py

---
+++ week2/code/dictionary.py
@@ -0,0 +1,39 @@
+taxa = [ ('Myotis lucifugus','Chiroptera'),
+         ('Gerbillus henleyi','Rodentia',),
+         ('Peromyscus crinitus', 'Rodentia'),
+         ('Mus domesticus', 'Rodentia'),
+         ('Cleithrionomys rutilus', 'Rodentia'),
+         ('Microgale dobsoni', 'Afrosoricida'),
+         ('Microgale talazaci', 'Afrosoricida'),
+         ('Lyacon pictus', 'Carnivora'),
+         ('Arctocephalus gazella', 'Carnivora'),
+         ('Canis lupus', 'Carnivora'),
+        ]
+
+# Write a python script to populate a dictionary called taxa_dic derived from
+# taxa so that it maps order names to sets of taxa and prints it to screen.
+# 
+# An example output is:
+#  
+# 'Chiroptera' : set(['Myotis lucifugus']) ... etc. 
+# OR, 
+# 'Chiroptera': {'Myotis  lucifugus'} ... etc
+
+taxa_dic = {}
+order_set = set()
+species_set = set()
+for i in taxa:
+        order_set.add(i[1])
+        species_set.add(i[0])
+
+taxa_dic.add{order_set, species_set}
+
+
+
+
+
+
+
+# Now write a list comprehension that does the same (including the printing after the dictionary has been created)  
+ 
+#### Your solution here #### 
