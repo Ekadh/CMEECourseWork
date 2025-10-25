7a39cdb25e7715d704c3a8dc2edda731974b676f
er925@ic.ac.uk <brinda@ekadhs-MacBook-Air.local>
2025-10-25 20:00:27 +0100

    Graph now renders and is correct but axis scales are different

--- week3/code/PP_Regress.R
+++ week3/code/PP_Regress.R
@@ -4,7 +4,9 @@ ecol_archives <- read.csv("../data/EcolArchives-E089-51-D1.csv")
 # Setting basic plot aesthetics
 p <- ggplot(ecol_archives, aes(log(Prey.mass), log(Predator.mass), color = Predator.lifestage))
 # Setting points and lm lines
-p <- p + geom_point(shape = I(2)) + geom_smooth(method = "lm", fullrange = TRUE)
+p <- p + geom_point(shape = I(3)) + geom_smooth(method = "lm", fullrange = TRUE)
 # Setting up facet wrap, labs and theme
-p <- p + facet_wrap(.~ Type.of.feeding.interaction) + labs(x = "Prey mass in grams", y = "Predator mass in grams") + theme_minimal()
+p <- p + facet_wrap(Type.of.feeding.interaction ~., ncol = 1, strip.position = "right") +
+  labs(x = "Prey mass in grams", y = "Predator mass in grams") +
+  theme(legend.position = "bottom")
 p
